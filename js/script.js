f965b996730002b0e79bbe08dbdd1d7bca50c7c8
Adappt <sowmya.s@outlook.in>
2017-08-28 18:47:58 +0530

    added textdata in list

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
 var API_KEY ="https://jsonplaceholder.typicode.com/todos";
 
-(function($) {
-	$.getJSON(API_KEY).then(function(data){	
-		$.each(data,function(key,value){
-			$("#todoList").append('<li><input type="checkbox" class="done"/>'+value.title+'</li>');
-		/*	$("#sortable").sortable();*/
-/*$("#sortable").disableSelection();*/
-		});
-	});
+//To display API values
+(function ($) {
+    $.getJSON(API_KEY).then(function (data) {
+        $.each(data, function (key, value) {
+        	//create checkbox for api list and add text in the begining using prepend
+            $("#todoList").prepend('<li><input type="checkbox" id="mycheckbox" class="done"/>' + value.title + '<span class="glyphicon glyphicon-pencil"></span></li>');
+            //completed task from api is marked in checkbox
+            $("#mycheckbox").prop('checked', value.completed);
+        });
+    });
 })(jQuery);
 
+$('#todoTextBox').keypress(function (event) {
+    event.stopPropagation(); //prevent the other events 
+    //display data while enter key pressed
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') { // keycode for enter
+        console.log('You pressed "enter key" in textbox');
+        if ($(this).val() != '') {
+            var todo = $(this).val();
+            console.log(todo);
+            createTodo(todo);
+        } else {
 
-
-countTodos();
-/*
-// all done btn
-$("#checkAll").click(function(){
-    AllDone();
-});*/
-
-//create todo
-$("#newtext").on('keypress',function (e) {
-     
-    countTodos();
-          
+        }
+    }
 });
+//To create todo
+function createTodo(text) {
+    var markup = '<li><input type="checkbox" id="mycheckbox" class="done"/>' + text + '<span class="glyphicon glyphicon-pencil"></span></li>';
+    $('#todoList').prepend(markup);
+    $("#todoTextBox").val('');
+}
+
+/*var option = {trigger : $("input.btn_edit"), action : "click"};
+$("span#edit").editable(option, function(e){
+  alert(e.value);
+});*/
