9c220aabefa862b774692b7338fb2eb94a26d685
Adappt <sowmya.s@outlook.in>
2017-09-02 18:42:52 +0530

    for post

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var API_KEY ="https://jsonplaceholder.typicode.com/todos";
+var POST_URL="http://192.168.1.29:1337/todos";
 
 //To display API values
 (function ($) {
@@ -9,15 +10,16 @@ var API_KEY ="https://jsonplaceholder.typicode.com/todos";
     });
 })(jQuery);
 
+
 $('#todoTextBox').keypress(function (event) {
     event.stopPropagation(); //prevent the other events 
     //display data while enter key pressed
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '13') { // keycode for enter
-        //console.log('You pressed "enter key" in textbox');
+        console.log('You pressed "enter key" in textbox');
         if ($(this).val() != '') {
             var todo = $(this).val();
-            //console.log(todo);
+            console.log(todo);
             createTodo(todo);
         } else {
 
@@ -26,20 +28,32 @@ $('#todoTextBox').keypress(function (event) {
 });
 //To create todo
 function createTodo(text) {	
-	$.ajax({
-  	type: 'POST',
-  	url: 'https://jsonplaceholder.typicode.com/todos',
-  	data: {title: text, completed: false},
-  	success: function(todoList) {
-    console.log("added!", todoList); //the new item is returned with an ID
-  }
-});
-    var markup = '<li><input type="checkbox" id="mycheckbox" class="done"/>' + text + '<span class="glyphicon glyphicon-pencil"></span></li>';
+    var toDo = $('#todoTextBox').val();
+
+	// POST adds a random id to the object sent
+             $.ajax(POST_URL, {              
+                 method: 'POST',
+                 data: {
+                  title: toDo
+                 }
+             }).then(function(data) {
+             	console.log("welcome page");
+             	console.log("My toDo"+toDo);
+          });
+// 	$.ajax({
+//   	type: 'POST',
+//   	url: 'POST_URL',
+//   	data: {title: text, completed: false},
+//   	success: function(todoList) {
+//     console.log("added!", todoList); //the new item is returned with an ID
+//   }
+// });
+    var markup = '<li><input type="checkbox" id="mycheckbox" class="done"/>' + text + '</li>';
     $('#todoList').prepend(markup);
     $("#todoTextBox").val('');
 }
 
-
+ 
 
 /*	console.log(data);
       /*var todoList = {target:data};*/
