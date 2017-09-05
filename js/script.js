ad7d51d66baaad5647d8ec516a2fe10ef5191650
Adappt <sowmya.s@outlook.in>
2017-09-05 19:11:41 +0530

    checkbox function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var API_KEY ="https://jsonplaceholder.typicode.com/todos";
 var POST_URL="http://192.168.1.29:1337/todos";
 var DESC="http://192.168.1.29:1337/todos?sort=id+desc";
+var PUT_URL="http://192.168.1.29:1337/todos/id+completed";
 
 //To display API values
 (function ($) {
@@ -9,15 +10,12 @@ var DESC="http://192.168.1.29:1337/todos?sort=id+desc";
     	var template = _.template( $("#tpl").text() );
     	$("#middle").html(template(result));	
     });
+
 })(jQuery);
 
-
-//To create todo
 function createTodo(text) {	
 	var toDo = $('#todoTextBox').val();
-	//console.log("POST_URL");
-	//console.log(POST_URL);
-	$.ajax('http://192.168.1.29:1337/todos', {
+	$.ajax(POST_URL, {
   	method: 'POST',
   	data:{
     title:toDo,
@@ -31,9 +29,31 @@ function createTodo(text) {
   	
   });
 
-   var markup = '<li><input type="checkbox" id="mycheckbox" class="done"/>' + text + '</li>';
+   var markup = '<li><input type="checkbox" id="mycheckbox" class="done" onchange="uplChk()" />' + text + '</li>';
     $('#todoList').prepend(markup);
     $("#todoTextBox").val('');
 }
+
+$(document).on( 'change', '.done', function(){
+    if($(this).is(":checked")){
+        alert("Checkbox is checked.");
+    }
+    else if($(this).is(":not(:checked)")){
+        alert("Checkbox is unchecked.");
+    }
+    var chk = $('#todoTextBox').val();
+    $.ajax(POST_URL, {
+  	method: 'PUT',
+ 	data: {
+    id: 15,
+    completed:true
+  }
+}).then(function(data) {
+  console.log(data);
+});
+
+    } );
+
+
 
 
