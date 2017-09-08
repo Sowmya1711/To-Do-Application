4bca2309a1bae89a55a16feb2316e41f77f68fa5
Adappt <sowmya.s@outlook.in>
2017-09-08 18:48:40 +0530

    es6

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-var API_KEY = "http://ec2-13-126-50-109.ap-south-1.compute.amazonaws.com:1337/todos";
-var POST_URL = "http://192.168.1.29:1337/todos/";
-var DESC = "http://192.168.1.29:1337/todos?sort=id+desc";
-var valTrue = "true";
-var valFalse = "false";
+let API_KEY = "http://ec2-13-126-50-109.ap-south-1.compute.amazonaws.com:1337/todos";
+let POST_URL = "http://192.168.1.29:1337/todos/";
+let DESC = "http://192.168.1.29:1337/todos?sort=id+desc";
+let valTrue = "true";
+let valFalse = "false";
 
 //To display API values
 (function ($) {
     $.getJSON(DESC).then(function (data) {
-        var result = { target: data };
-        var template = _.template($("#tpl").text());
+        let result = { target: data };
+        let template = _.template($("#tpl").text());
         $("#middle").html(template(result));
     });
 })(jQuery);
 
 //For POST to api
 function createTodo(text) {
-    var toDo = $('#todoTextBox').val();
+    let toDo = $('#todoTextBox').val();
     $.ajax(POST_URL, {
         method: 'POST',
         data: {
@@ -30,14 +30,14 @@ function createTodo(text) {
 
     });
 
-    var markup = '<li><input type="checkbox" id="mycheckbox" class="done" onchange="uplChk()" />' + text + '</li>';
+    let markup = '<li><input type="checkbox" id="mycheckbox" class="done" onchange="uplChk()" />' + text + '</li>';
     $('#todoList').prepend(markup);
     $("#todoTextBox").val('');
 }
 
 function updateCheck() {
     $('input[type="checkbox"]').change(function () {
-        var checked = false;
+        let checked = false;
         if ($(this).prop("checked") == true) {
             checked = true;
         }
@@ -50,8 +50,8 @@ function updateCheck() {
 
 function createCheck(id, checked) {
 
-    var PUT_LAST = "?completed=";
-    var PUT_URLL = POST_URL + id;
+    let PUT_LAST = "?completed=";
+    let PUT_URLL = POST_URL + id;
     $.ajax(PUT_URLL, {
         method: 'PUT',
         data: {
